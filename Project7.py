b5edf5c14f0230f014f5d7fa5d93d14520c6426d
Abderahman Mohamed <145558673+abdoabozena7@users.noreply.github.com>
2025-09-08 18:17:32 +0300

    upload

---
+++ Project7.py
@@ -0,0 +1,7 @@
+
+print("Hello, World!")
+
+print("welcome to youtube channel generator")
+name =input( "what is your name \n")
+about =input( "what is your channel about \n")
+print ( "you cloud name your channel "+(name)+( about)) 
---
+++ Project9.py
@@ -0,0 +1,6 @@
+
+print("Hello, World!")
+d=input("write two digits you wanna to sum \n")
+f=int(d[0])
+s=int(d[1])
+print(f+s)
